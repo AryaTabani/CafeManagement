03f949df767a847590b5a2803ab306453eb8591c
AryaTabani <a.tabani82@gmail.com>
2025-07-24 12:10:34 +0330

    implementing database

--- DB/db.go
+++ DB/db.go
@@ -2,8 +2,10 @@ package db
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var DB *sql.DB
@@ -18,6 +20,7 @@ func InitDB() {
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
 	createtables()
+	createDefaultAdmin()
 }
 
 func createtables() {
@@ -49,5 +52,40 @@ func createtables() {
 	_, err = DB.Exec(createMenuItemsTable)
 	if err != nil {
 		panic("Failed to create menu_items table")
+	}
+	createAdminsTable := `
+    CREATE TABLE IF NOT EXISTS admins (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL
+    );`
+
+	_, err = DB.Exec(createAdminsTable)
+	if err != nil {
+		panic("Failed to create admins table")
+	}
+
+}
+func createDefaultAdmin() {
+	defaultUsername := "admin"
+	defaultPassword := "admin123"
+
+	var count int
+	err := DB.QueryRow("SELECT COUNT(*) FROM admins WHERE username = ?", defaultUsername).Scan(&count)
+	if err != nil {
+		panic("Failed to check for default admin: " + err.Error())
+	}
+	if count == 0 {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(defaultPassword), bcrypt.DefaultCost)
+		if err != nil {
+			panic("Failed to hash default password: " + err.Error())
+		}
+		query := `INSERT INTO admins (username, password_hash) VALUES (?, ?)`
+		_, err = DB.Exec(query, defaultUsername, string(hashedPassword))
+		if err != nil {
+			panic("Failed to create default admin user: " + err.Error())
+		}
+		log.Printf("   Username: %s\n", defaultUsername)
+		log.Printf("   Password: %s\n", defaultPassword)
 	}
 }
